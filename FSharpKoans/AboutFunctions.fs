427369648619fdfd836ad77d8dd9e55c0c6e1036
Lubabalo Buwa <G17B3155@ict.ru.ac.za>
2019-02-28 16:41:30 +0200

    Added FSharpKoans Files

--- FSharpKoans/AboutFunctions.fs
+++ FSharpKoans/AboutFunctions.fs
@@ -18,15 +18,15 @@ open NUnit.Framework
 module ``03: Putting the Function into Functional Programming`` = 
     [<Test>]
     let ``01 A function takes one input and produces one output`` () =
-        (fun a -> a + 100) __ |> should equal 2097
+        (fun a -> a + 100) 1997 |> should equal 2097
 
     [<Test>]
     let ``02 The input to a function is a pattern (Part 1).`` () =
-        (fun 7 -> 9) __ |> should equal 9
+        (fun 7 -> 9) 7 |> should equal 9
 
     [<Test>]
     let ``03 The input to a function is a pattern (Part 2).`` () =
-        (fun _ -> 75) __ |> should equal 75
+        (fun _ -> 75) 60 |> should equal 75
 
     [<Test>]
     let ``04 The input to a function is a pattern (Part 3).`` () =
